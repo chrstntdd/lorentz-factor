acc5c959a1b1710cfc64a755f95a9df320a03f5b
Christian Todd <ChristianTodd27@icloud.com>
2016-05-03 20:03:43 -0400

    Updated comments and added an error messagge for improper inputs

--- GAMMA.m
+++ GAMMA.m
@@ -6,5 +6,7 @@ if v<1;
 elseif v>1;
     v=v/299792458;
     [y]=Gamma_Function(v);
+elseif v==1;
+    disp('ERROR: Can not travel at the speed of light (yet).');
 end
 fprintf('The relativistic gamma factor is = %0.4G', y);
--- Gamma_Function.m
+++ Gamma_Function.m
@@ -1,5 +1,5 @@
 function y=Gamma_Function(v)
 %Input: Velocity
-%Output: Relativistic Gamma Factor
-c = 299792458;  % Constant (speed of light in meters per second)
-y= 1/sqrt(1-((v*c)^2/c^2));  %Gamma function
+%Output: Lorentz Factor
+c = 299792458;  % Constant (speed of light measured in meters per second)
+y= 1/sqrt(1-((v*c)^2/c^2));  %Lorentz factor definition
